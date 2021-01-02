e78f8506b78727b43773992f2896c47a2d8c556a
rakesh <rakesh@forsfortis.net>
2021-01-02 17:46:12 -0500

    spell fic

--- src/main/java/com/forsfortis/incmanager/SecurityConfiguration.java
+++ src/main/java/com/forsfortis/incmanager/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,10 +22,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
        http.authorizeRequests()
-            .antMatchers("/admin").hasRole("USER")
-            .antMatchers("/user").hasRole("USER")
-            .antMatchers("/").permitAll()
+       .anyRequest().authenticated()
+            /*.antMatchers("/admin").hasRole("ADMIN")
+            .antMatchers("/").hasRole("USER")*/
+           // .antMatchers("/").permitAll()
             .and().formLogin();
+       /*   .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
+                .permitAll(); */
     }
 
     @Bean
